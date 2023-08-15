de6cd211c0fd2910af2d730515b765d2b5012a63
VishalDhanasek <veeruvishal4@gmail.com>
2023-08-15 10:49:55 +0530

    added privacy policy

--- components/ui/footer.tsx
+++ components/ui/footer.tsx
@@ -71,7 +71,7 @@ export default function Footer() {
                 <a href="#0" className="text-gray-600 hover:text-gray-900 transition duration-150 ease-in-out">Pricing</a>
               </li>
               <li className="mb-2">
-                <a href="#0" className="text-gray-600 hover:text-gray-900 transition duration-150 ease-in-out">Privacy Policy</a>
+                <a href="/privacypolicy" className="text-gray-600 hover:text-gray-900 transition duration-150 ease-in-out">Privacy Policy</a>
               </li>
             </ul>
           </div>
